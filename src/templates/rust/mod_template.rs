e9129d13342a154453dfe8abf072081b385fd053
Abdullahsab3 <abdallah-sab3@hotmail.com>
2023-12-25 11:27:00 +0100

    module templates

---
+++ src/templates/rust/mod_template.rs
@@ -0,0 +1,34 @@
+use crate::utils::naming_convention::{to_snake_case_plural, to_snake_case};
+
+pub static MOD_TEMPLATE: &str = r#"
+pub mod {module_name};
+"#;
+
+pub static CONTROLLER_MOD_TEMPLATE: &str = r#"
+{sc_entity_plural}_controller
+"#;
+
+pub static MODEL_MOD_TEMPLATE: &str = r#"
+{sc_entity_name}
+"#;
+
+pub static SERVICE_MOD_TEMPLATE: &str = r#"
+{sc_entity_name}_service
+"#;
+
+pub trait ModGenerator {
+    fn generate_controller_mod(&self, entity_name: &str) -> String {
+        let module_name = CONTROLLER_MOD_TEMPLATE.replace("{sc_entity_plural}", &to_snake_case_plural(entity_name));
+        MOD_TEMPLATE.replace("{module_name}", &module_name)
+    }
+
+    fn generate_model_mod(&self, entity_name: &str) -> String {
+        let module_name = MODEL_MOD_TEMPLATE.replace("{sc_entity_name}", &to_snake_case(entity_name));
+        MOD_TEMPLATE.replace("{module_name}", &module_name)
+    }
+    
+    fn generate_service_mod(&self, entity_name: &str) -> String {
+        let module_name = SERVICE_MOD_TEMPLATE.replace("{sc_entity_name}", &to_snake_case(entity_name));
+        MOD_TEMPLATE.replace("{module_name}", &module_name)
+    }
+}
